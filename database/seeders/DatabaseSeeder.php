fbce57ee91d080e6371bfbc9383800bfbf943bf6
MariaP <maria.pirinova39@gmail.com>
2025-03-20 16:28:49 +0200

    Create database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Nette\Utils\Random;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::table('job_listings')->truncate();
+        DB::table('users')->truncate();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(RandomUserSeeder::class);
+        $this->call(JobSeeder::class);
     }
 }
