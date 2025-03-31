2368ecd782dd63dadf243ec546c27f2080ca4539
MariaP <maria.pirinova39@gmail.com>
2025-03-31 23:25:20 +0300

    Add alert component and add Alpine.js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
+
 <body class="bg-gray-100">
     <x-header />
     @if(request()->is('/'))
@@ -16,8 +21,15 @@
     <x-top-banner />
     @endif
     <main class="container mx-auto p-4 mt-4">
+        @if(session('success'))
+        <x-alert type="success" message="{{session('success')}}" timeout="2000" />
+        @endif
+        @if(session('error'))
+        <x-alert type="error" message="{{session('error')}}" />
+        @endif
         {{$slot}}
     </main>
     <script src="{{asset('js/script.js')}}"></script>
 </body>
+
 </html>
