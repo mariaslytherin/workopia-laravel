bacf1f879c7a6129f89db37f32e06106a7be1d2c
MariaP <maria.pirinova39@gmail.com>
2025-04-02 23:59:37 +0300

    Implement logout functionality

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -32,4 +32,14 @@ class LoginController extends Controller
             'email' => 'The provided credentials do not match our records'
         ])->onlyInput('email');
     }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
