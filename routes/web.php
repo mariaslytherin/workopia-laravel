472b534f99d2c24f4e53d302611273786aabacee
MariaP <maria.pirinova39@gmail.com>
2025-04-02 23:29:55 +0300

    Implement register controller and view

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisterController::class, 'register'])->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
